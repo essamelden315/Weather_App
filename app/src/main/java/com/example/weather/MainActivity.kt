752f33802d06ecbd6ff676319cedc5722986e1c4
essam <essameldenmohamed@yahoo.com>
2023-03-20 04:23:40 +0200

    "starting ui"

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
+    lateinit var cons:ConstraintLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        cons = findViewById(R.id.CL)
+        cons.setBackgroundResource(R.drawable.gradient_night_background)
+        window.statusBarColor = getColor(R.color.nightStart)
     }
 }
