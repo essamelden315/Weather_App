47948bdb4bde7568e6e81dc185fbd954c217c43e
essam <essameldenmohamed@yahoo.com>
2023-03-26 22:03:54 +0200

    "map is working"

--- app/src/main/java/com/example/weather/MapsFragment.kt
+++ app/src/main/java/com/example/weather/MapsFragment.kt
@@ -3,6 +3,7 @@ package com.example.weather
 import android.location.Address
 import android.location.Geocoder
 import android.os.Bundle
+import android.util.Log
 import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
@@ -20,25 +21,19 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 
-class MapsFragment : Fragment() {
+class MapsFragment : Fragment(){
     lateinit var fusedClient: FusedLocationProviderClient
     lateinit var binding: FragmentMapsBinding
     lateinit var mapFragment: SupportMapFragment
     lateinit var mMap:GoogleMap
+
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
         mMap = googleMap
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.setOnMapClickListener {
+            mMap.clear()
+            mMap.addMarker(MarkerOptions().position(it))
+            goToLatLng(it.latitude,it.longitude,16f)
+        }
     }
 
     override fun onCreateView(
@@ -57,7 +52,7 @@ class MapsFragment : Fragment() {
         val locationRequest:LocationRequest = LocationRequest()
         locationRequest.setInterval(5000)
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-        locationRequest.setSmallestDisplacement(16f)
+        locationRequest.setSmallestDisplacement(14f)
         locationRequest.setFastestInterval(3000)
         binding.searchEdt.setOnEditorActionListener(OnEditorActionListener { v, actionId, event ->
             if(actionId==EditorInfo.IME_ACTION_SEARCH ||actionId==EditorInfo.IME_ACTION_DONE
@@ -74,23 +69,23 @@ class MapsFragment : Fragment() {
 
     private fun goToSearchLocation() {
         var searchLocation = binding.searchEdt.text.toString()
-        var geocoder:Geocoder = Geocoder(requireContext())
-        var list =geocoder.getFromLocationName(searchLocation,1)
+        var list =Geocoder(requireContext()).getFromLocationName(searchLocation,1)
         if(list != null && list.size > 0){
             var address:Address = list.get(0)
-            var location:String = address.adminArea
-            var latitude = address.latitude
-            var longitude = address.longitude
-            goToLatLng(latitude,longitude,14f)
+            goToLatLng(address.latitude,address.longitude,16f)
         }
 
     }
 
     private fun goToLatLng(latitude: Double, longitude: Double, fl: Float) {
-        var latlng:LatLng = LatLng(latitude,longitude)
+        Log.i("mapCoordinates", "${latitude} ${longitude}")
+        var geocoder = Geocoder(requireContext()).getFromLocation(latitude,longitude,1)
+        Log.i("geoCoder", "${geocoder?.get(0)?.subAdminArea}, ${geocoder?.get(0)?.adminArea}")
+        var latlng = LatLng(latitude,longitude)
         var update: CameraUpdate = CameraUpdateFactory.newLatLngZoom(latlng,fl)
-
+        mMap.addMarker(MarkerOptions().position(latlng))
         mMap.animateCamera(update)
+
     }
 
 
